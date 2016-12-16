0f57632d6bec36f71d36471f21b00c4b04933b43
lz <shyylz@126.com>
2016-12-16 14:31:19 +0800

    增加dom组件和template组件

---
+++ mirror.dom.js
@@ -0,0 +1,6 @@
+/**
+ * Created by shyy_work on 2016/12/16.
+ */
+;(function(m){
+
+})(mirror);
---
+++ mirror.template.js
@@ -0,0 +1,9 @@
+/**
+ * Created by shyy_work on 2016/12/16.
+ */
+(function(m){
+    var Template = function(model,parent){
+        this.model = model;
+        this.parent = parent;
+    };
+})(mirror);
