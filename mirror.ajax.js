8ad0a53166ce6f3a43d82ae0884d9b6f31bea1eb
lz <shyylz@126.com>
2016-12-16 12:04:22 +0800

    ajax 基本功能移植

--- mirror.ajax.js
+++ mirror.ajax.js
@@ -5,34 +5,124 @@
     var logEvent = function (event) {
             m.log(event);
         },
+        //from jquery
+        rhash = /#.*$/,
+        defaultConfig = {
+            url: location.href,
+            global: true,
+            method: "GET",
+            contentType: "application/x-www-form-urlencoded",
+            processData: true,
+            async: true,
+            timeout: 10000,
+            xhr: function () {
+                return new m.window.XMLHttpRequest();
+            },
+            responseFields: {xml: 'responseXML', text: "responseText"},
+            contents: {"html": /html/, "json": /json/, "script": /javascript|ecmascript/, "xml": /xml/},
+            converters: {
+                "* text": m.stringify,
+                "text html": true,
+                "text json": m.parseJSON,
+                "text xml": m.parseXML
+            },
+            accepts: {
+                xml: "application/xml, text/xml",
+                html: "text/html",
+                text: "text/plain",
+                json: "application/json, text/javascript",
+                "*": ["*/"] + ["*"]
+            },
+        }, hooksExecutor = function (hooks, event) {
+            if(!hooks)return;
+            for (var i = 0; i < hooks.length; i++) {
+                (hooks[i])(event);
+            }
+        },
         core_rspace = /\s+/,
+        rheaders = /^(.*?):[ \t]*([^\r\n]*)\r?$/mg,
+        preparedResponse = function (xhr, config) {
+            var ct, type, finalDataType, firstDataType, responseHeadersString = xhr.getAllResponseHeaders(), response = {}, responseHeaders = {}, match;
+            while (( match = rheaders.exec(responseHeadersString) )) {
+                responseHeaders[match[1].toLowerCase()] = match[2];
+            }
+            for (type in config.responseFields) {
+                if (xhr[config.responseFields[type]]) {
+                    response[type] = xhr[config.responseFields[type]];
+                }
+            }
+            while (config.dataTypes[0] === "*") {
+                config.dataTypes.shift();
+                if (ct === undefined) {
+                    ct = config.mimeType || responseHeaders["content-type"];
+                }
+            }
+            if (ct) {
+                for (type in config.contents) {
+                    if (config.contents[type] && config.contents[type].test(ct)) {
+                        config.dataTypes.unshift(type);
+                        break;
+                    }
+                }
+            }
+            if (config.dataTypes[0] in response) {
+                finalDataType = config.dataTypes[0];
+            } else {
+                // Try convertible dataTypes
+                for (type in response) {
+                    if (!config.dataTypes[0] || config.converters[type + " " + config.dataTypes[0]]) {
+                        finalDataType = type;
+                        break;
+                    }
+                    if (!firstDataType) {
+                        firstDataType = type;
+                    }
+                }
+                // Or just use first one
+                finalDataType = finalDataType || firstDataType;
+            }
+            if (finalDataType) {
+                if (finalDataType !== config.dataTypes[0]) {
+                    config.dataTypes.unshift(finalDataType);
+                }
+            }
+            return response = m.extend(response, {
+                responseHeadersString: responseHeadersString,
+                responseHeaders: responseHeaders,
+                responseType: xhr.responseType,
+                responseURL: xhr.responseURL,
+                status: xhr.status
+            });
+        },
         ajaxConverter = function (config, response) {
             var conv, conv2, current, tmp,
                 dataTypes = config.dataTypes.slice(),
-                prev = dataTypes[ 0 ],
+                prev = dataTypes[0],
                 converters = {},
                 i = 0;
-            if ( dataTypes[ 1 ] ) {
-                for ( conv in config.converters ) {
-                    converters[ conv.toLowerCase() ] = config.converters[ conv ];
+            if (dataTypes[1]) {
+                for (conv in config.converters) {
+                    converters[conv.toLowerCase()] = config.converters[conv];
                 }
+            }else{
+                response.responseData = response[prev];
             }
-            for ( ; (current = dataTypes[++i]); ) {
-                if ( current !== "*" ) {
-                    if ( prev !== "*" && prev !== current ) {
-                        conv = converters[ prev + " " + current ] || converters[ "* " + current ];
-                        if ( !conv ) {
-                            for ( conv2 in converters ) {
+            for (; (current = dataTypes[++i]);) {
+                if (current !== "*") {
+                    if (prev !== "*" && prev !== current) {
+                        conv = converters[prev + " " + current] || converters["* " + current];
+                        if (!conv) {
+                            for (conv2 in converters) {
                                 tmp = conv2.split(" ");
-                                if ( tmp[ 1 ] === current ) {
-                                    conv = converters[ prev + " " + tmp[ 0 ] ] ||
-                                        converters[ "* " + tmp[ 0 ] ];
-                                    if ( conv ) {
-                                        if ( conv === true ) {
-                                            conv = converters[ conv2 ];
-                                        } else if ( converters[ conv2 ] !== true ) {
-                                            current = tmp[ 0 ];
-                                            dataTypes.splice( i--, 0, current );
+                                if (tmp[1] === current) {
+                                    conv = converters[prev + " " + tmp[0]] ||
+                                        converters["* " + tmp[0]];
+                                    if (conv) {
+                                        if (conv === true) {
+                                            conv = converters[conv2];
+                                        } else if (converters[conv2] !== true) {
+                                            current = tmp[0];
+                                            dataTypes.splice(i--, 0, current);
                                         }
 
                                         break;
@@ -40,18 +130,22 @@
                                 }
                             }
                         }
-                        if ( conv !== true ) {
+                        if (conv !== true) {
                             try {
-                                response.responseData = conv( response[prev]);
-                            } catch ( e ) {
-                                return { state: "parsererror", error: conv ? e : "No conversion from " + prev + " to " + current };
+                                response.responseData = conv(response[prev]);
+                            } catch (e) {
+                                response.isSucceed = false;
+                                response.e = "No conversion from " + prev + " to " + current;
+                                return;
                             }
+                        }else{
+                            response.responseData = response[prev];
                         }
                     }
                     prev = current;
                 }
             }
-            return { state: "success", data: response };
+            response.isSucceed = true;
         };
     var AjaxObserver = function (preparedHooks, startedHooks, completedHooks, successedHooks, failedHooks) {
         this.preparedHooks = preparedHooks || [logEvent];
@@ -69,7 +163,7 @@
     AjaxObserver.prototype.onCompleted = function (event) {
         hooksExecutor(this.completedHooks, event);
     };
-    AjaxObserver.prototype.onSuccessed = function (event) {
+    AjaxObserver.prototype.onSucceed = function (event) {
         hooksExecutor(this.successedHooks, event);
     };
     AjaxObserver.prototype.onFailed = function (event) {
@@ -84,85 +178,43 @@
         if (this.config.async) {
             this.xhr.timeout = this.config.timeout;
             m.inject(this).before("start", function (jp) {
-                observer.onPrepared({config: this.config, readyState: this.xhr.readyState, type: 'prepared'});
+                observer.onPrepared({config: this.config, type: 'prepared'});
             }).after("start", function (jp) {
-                observer.onStarted({config: this.config, readyState: this.xhr.readyState, type: 'started'});
+                observer.onStarted({config: this.config, type: 'started'});
             }).flush();
             var config = this.config;
             this.xhr.onreadystatechange = function (e) {
                 if (this.readyState === 4) {
-                    var ct,type,finalDataType,firstDataType,responseHeadersString = this.getAllResponseHeaders(),response = {}, responseHeaders = {}, match, rheaders = /^(.*?):[ \t]*([^\r\n]*)\r?$/mg;
-                    while (( match = rheaders.exec(responseHeadersString) )) {
-                        responseHeaders[match[1].toLowerCase()] = match[2];
-                    }
-                    for(type in config.responseFields){
-                        if (this[config.responseFields[type]]) {
-                            response[type] = this[config.responseFields[type]];
-                        }
-                    }
-                    while( config.dataTypes[ 0 ] === "*" ) {
-                        config.dataTypes.shift();
-                        if ( ct === undefined ) {
-                            ct = config.mimeType || responseHeaders["content-type"];
-                        }
-                    }
-                    if ( ct ) {
-                        for ( type in config.contents ) {
-                            if ( config.contents[ type ] && config.contents[ type ].test( ct ) ) {
-                                config.dataTypes.unshift( type );
-                                break;
-                            }
-                        }
-                    }
-                    if ( config.dataTypes[ 0 ] in response ) {
-                        finalDataType = config.dataTypes[ 0 ];
-                    } else {
-                        // Try convertible dataTypes
-                        for ( type in response ) {
-                            if ( !config.dataTypes[ 0 ] || config.converters[ type + " " + config.dataTypes[0] ] ) {
-                                finalDataType = type;
-                                break;
-                            }
-                            if ( !firstDataType ) {
-                                firstDataType = type;
-                            }
-                        }
-                        // Or just use first one
-                        finalDataType = finalDataType || firstDataType;
-                    }
-                    if ( finalDataType ) {
-                        if ( finalDataType !== config.dataTypes[ 0 ] ) {
-                            config.dataTypes.unshift( finalDataType );
-                        }
-                    }
-                    response = m.extend(response,{
-                        responseHeadersString: responseHeadersString,
-                        responseHeaders: responseHeaders,
-                        responseType: this.responseType,
-                        responseURL: this.responseURL,
-                        status : this.readyState
-                    });
+                    var response = preparedResponse(this, config)
                     observer.onCompleted({type: 'completed', response: response});
-                    if (ajaxConverter(config, response)) {
-                        observer.onSuccessed({});
+                    ( this.status >= 200 && this.status < 300 || this.status === 304 )&&ajaxConverter(config, response);
+                    if (response.isSucceed) {
+                        observer.onSucceed(response.responseData);
                     } else {
-                        observer.onFailed({});
+                        observer.onFailed(response);
                     }
-
                 }
             };
             this.xhr.ontimeout = function (e) {
-                observer.onFailed({});
+                observer.onFailed({type: 'timeout', time: config.timeout});
             }
         } else {
             m.inject(this).before("start", function (jp) {
-                observer.onPrepared({});
+                observer.onPrepared({config: this.config, type: 'prepared'});
             }).after("start", function (jp) {
-                observer.onStarted({});
+                observer.onStarted({config: this.config, type: 'started'});
             }).after("send", function (jp) {
-                observer.onCompleted({});
-                observer.onSuccessed({});
-                observer.onFailed({});
+                if (this.xhr.readyState === 4) {
+                    var response = preparedResponse(this.xhr, this.config)
+                    observer.onCompleted({type: 'completed', response: response});
+                    ( this.xhr.status >= 200 && this.xhr.status < 300 || this.xhr.status === 304 )&&ajaxConverter(this.config, response);
+                    if (response.isSucceed) {
+                        observer.onSucceed(response.responseData);
+                    } else {
+                        observer.onFailed(response);
+                    }
+
+                }
             }).flush();
         }
         return this;
@@ -183,48 +235,27 @@
     AjaxTask.prototype.send = function () {
         this.xhr.send();
     };
-    //from jquery
-    var rhash = /#.*$/,
-        defaultConfig = {
-            url: location.href,
-            global: true,
-            method: "GET",
-            contentType: "application/x-www-form-urlencoded",
-            processData: true,
-            async: true,
-            timeout: 10000,
-            xhr: function () {
-                return new window.XMLHttpRequest();
-            },
-            responseFields : {xml:'responseXML',text:"responseText"},
-            contents: {"html": /html/,"json":/json/,"script":/javascript|ecmascript/,"xml": /xml/},
-            converters: {
-                "* text": m.stringify,
-                "text html": true,
-                "text json": m.parseJSON,
-                "text xml": m.parseXML
-            },
-            accepts: {
-                xml: "application/xml, text/xml",
-                html: "text/html",
-                text: "text/plain",
-                json: "application/json, text/javascript",
-                "*": ["*/"]+["*"]
-            },
-        }, hooksExecutor = function (hooks, event) {
-            for (var i = 0; i < hooks.length; i++) {
-                (hooks[i])(event);
-            }
-        };
+    var megreConfig = function(cfg){
+        var config = m.extend({}, defaultConfig);
+        config = m.extend(config, cfg);
+        config.url = config.url.replace(rhash, "");
+        return config;
+    };
     m.extend({
         ajaxSetup: function (config) {
             return m.extend(defaultConfig, config);
         },
         ajax: function (cfg) {
-            var config = m.extend({}, defaultConfig);
-            config = m.extend(config, cfg);
-            config.url = config.url.replace(rhash, "");
-            new AjaxTask(config).adapted(new AjaxObserver()).start().send();
+            var config = megreConfig(cfg);
+            new AjaxTask(config).adapted(new AjaxObserver(config.prepared,config.started,config.completed,config.succeed,config.failed)).start().send();
+        },
+        getXML: function (cfg) {
+            cfg.dataTypes = "xml";
+            m.ajax(cfg);
+        },
+        getJSON: function(cfg){
+            cfg.dataTypes = "json";
+            m.ajax(cfg);
         }
     });
 })(window.mirror);
