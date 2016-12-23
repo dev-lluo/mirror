dba6cea7760d89175873b4775f0e66a698ad3e81
lz <shyylz@126.com>
2016-12-23 15:09:26 +0800

    op template

--- mirror.template.js
+++ mirror.template.js
@@ -2,40 +2,57 @@
  * Created by shyy_work on 2016/12/16.
  */
 (function(m){
-    function VDom(){
-
+    var d = m.using("mirror.dom"),
+        template = {};
+    function analyzeNode(node) {
+        var vd = new VDom(node);
+        m.each(d.getChild(node,true),function () {
+            vd.push(analyzeNode(this));
+        });
+        return vd;
+    }
+    function VDom(node){
+        this.node = node;
     }
     VDom.prototype = [];
-    VDom.prototype.each = function (func) {
-
+    VDom.prototype.dataBinding = function (data) {
+        var realNode = d.cloneNode(this.node);
+        m.each(this,function () {
+            d.append(realNode,this.dataBinding(data));
+        });
+        return realNode;
     }
     function Template(model,parent){
         this.model = model;
         this.parent = parent;
         this.initd = false;
     }
+    Template.prototype = [];
     Template.prototype.init = function () {
-        var model = d.valueOf(this.model);
+        var dom = d.valueOf(this.model)
+            ,container = this;
+        dom.each(function () {
+            container.push(analyzeNode(this));
+        });
+        this.initd = true;
     };
     Template.prototype.renderTo = function (selector, data) {
         if(!this.initd){
             this.init();
         }
         var parent,child;
-        this.model.each(function(){
-            child = this;
+        m.each(this,function () {
+            child = this.dataBinding(data);
             d.dom(selector).each(function () {
                 parent = this;
                 d.append(parent,child);
             });
         })
     };
-    var d = m.using("mirror.dom"),
-        template = {};
     d.dom("script[type='text/template']").each(function () {
         var id = d.getAttr(this,"id");
         m.assertDefined(id);
-        template[id] = new Template(d.getText(this));
+        template[id] = new Template(m.trim(d.getText(this)));
     });
     return template;
 })(mirror);
