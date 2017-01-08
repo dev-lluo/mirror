d5474f884718325688462e32af3a36d91214ab5d
devll <dev.lluo@outlook.com>
2017-01-08 11:21:26 +0800

    测试template

--- mirror.dom.js
+++ mirror.dom.js
@@ -1,87 +1,114 @@
 /**
  * Created by shyy_work on 2016/12/16.
  */
-;(function(m){
+;(function (m) {
     var sizzle = mirror.using("mirror.sizzle")
-        ,eventHook = {
+        , eventHook = {
             input: "input"
         }
-        ,attrHook = {
-        }
-        ,isEmptyExp = (function(exp){
-            m.inject(exp).after("test",function () {
+        , attrHook = {}
+        , isEmptyExp = (function (exp) {
+            m.inject(exp).after("test", function () {
                 this.restore();
             }).flush();
             return exp;
-        })(/^\s*$/g);;
-    function Dom(selector,context) {
-        if(m.isOne(selector,"String")){
-            sizzle(selector,context,this);
-        }else if(m.isOne(selector,"Array")){
-            this.push.apply(this,selector);
+        })(/^\s*$/g);
+
+    function Dom(selector, context) {
+        if (m.isOne(selector, "String")) {
+            sizzle(selector, context, this);
+        } else if (m.isOne(selector, "Array")) {
+            this.push.apply(this, selector);
         }
     };
     Dom.on = function (dom, type, func, useCapture) {
-        dom.addEventListener(eventHook[type]||type,func,useCapture||false);
+        dom.addEventListener(eventHook[type] || type, func, useCapture || false);
     };
-    Dom.off = function(dom,type,func,useCapture){
-        dom.removeEventListener(eventHook[type]||type, func,useCapture||false);
+    Dom.off = function (dom, type, func, useCapture) {
+        dom.removeEventListener(eventHook[type] || type, func, useCapture || false);
     };
-    Dom.getText = function(dom){
-        var results = [];
-        m.each(dom.childNodes,function(){
-            results.push(this.nodeValue);
-        },function(){return this.nodeType===3&&!isEmptyExp.test(this.nodeValue)});
-        return results.join('');
-    };
-    Dom.setText = function(dom,text){
-        var did = false;
-        m.each(dom.childNodes,function(){
-            if(did){
-                this.nodeValue = "";
-            }else{
-                this.nodeValue = text;
-                did = true;
-            }
-        },function(){return this.nodeType===3&&!isEmptyExp.test(this.nodeValue)});
-    };
-    Dom.setValue = function(dom,val){
-        dom.value = val;
-    };
-    Dom.getValue = function(dom){
-        return dom.value;
-    };
-    Dom.removeAttr = function(dom,attr){
-        return dom.removeAttribute(attrHook[attr]||attr);
-    };
-    Dom.getAttr = function(dom,attr){
-        return dom.getAttribute(attrHook[attr]||attr);
-    };
-    Dom.setAttr = function(dom,attr,val){
-        if(val!==Dom.getAttr(dom,attr)){
-            dom.setAttribute(attrHook[attr]||attr,val);
+    Dom.getText = function (dom) {
+        if (dom.nodeType === 3) {
+            return dom.nodeValue
+        } else {
+            var results = [];
+            m.each(dom.childNodes, function () {
+                results.push(this.nodeValue);
+            }, function () {
+                return this.nodeType === 3 && !isEmptyExp.test(this.nodeValue)
+            });
+            return results.join('');
         }
     };
-    Dom.getChild = function(dom,hasText){
+    Dom.setText = function (dom, text) {
+        if (dom.nodeType === 3) {
+            dom.nodeValue = text;
+        } else {
+            var did = false;
+            m.each(dom.childNodes, function () {
+                if (did) {
+                    this.nodeValue = "";
+                } else {
+                    this.nodeValue = text;
+                    did = true;
+                }
+            }, function () {
+                return this.nodeType === 3 && !isEmptyExp.test(this.nodeValue)
+            });
+        }
+    };
+    Dom.setValue = function (dom, val) {
+        dom.value = val;
+    };
+    Dom.getValue = function (dom) {
+        return dom.value;
+    };
+    Dom.removeAttr = function (dom, attr) {
+        return dom.removeAttribute(attrHook[attr] || attr);
+    };
+    Dom.hasAttr = function (dom, attr) {
+        return dom.hasAttribute(attrHook[attr] || attr);
+    };
+    var attrPolyfill = {
+        text: {
+            get: Dom.getText,
+            set: Dom.setText
+        },
+        value: {
+            get: Dom.getValue,
+            set: Dom.setValue
+        }
+    };
+    Dom.getAttr = function (dom, attr) {
+        return attrPolyfill[attr] ? attrPolyfill[attr].get(dom) : dom.getAttribute(attrHook[attr] || attr);
+    };
+    Dom.setAttr = function (dom, attr, val) {
+        if (val !== Dom.getAttr(dom, attr)) {
+            attrPolyfill[attr] ? attrPolyfill[attr].set(dom, val) : dom.setAttribute(attrHook[attr] || attr, val);
+        }
+    };
+    Dom.getChild = function (dom, hasText) {
         var child = [];
-        m.each(dom.childNodes,function(){
+        m.each(dom.childNodes, function () {
             child.push(this);
-        },function(){return this.nodeType===1||(hasText&&this.nodeType===3);});
+        }, function () {
+            return this.nodeType === 1 || (hasText && this.nodeType === 3);
+        });
         return child;
     };
-    Dom.getParent  = function(dom){
+    Dom.getParent = function (dom) {
         return dom.parentNode;
     };
-    Dom.append = function(parent,child){
+    Dom.append = function (parent, child) {
         parent.appendChild(child);
     };
-    Dom.remove = function(child){
+    Dom.remove = function (child) {
         child.parentNode.removeChild(child);
     };
-    Dom.replace = function(oldDom,newDom){
-        oldDom.parentNode.replaceChild(newDom,oldDom);
+    Dom.replace = function (oldDom, newDom) {
+        oldDom.parentNode.replaceChild(newDom, oldDom);
     };
-    Dom.cloneNode = function(dom,dp){
+    Dom.cloneNode = function (dom, dp) {
         return dom.cloneNode(dp);
     };
     Dom.valueOf = function (str) {
@@ -90,134 +117,126 @@
     };
     Dom.prototype = [];
     Dom.prototype.each = function (func) {
-        m.each(this,func);
+        m.each(this, func);
         return this;
     };
-    Dom.prototype.on = function (type,func,useCapture) {
+    Dom.prototype.on = function (type, func, useCapture) {
         return this.each(function () {
-            Dom.on(this,type,func,useCapture);
+            Dom.on(this, type, func, useCapture);
         })
     };
     Dom.prototype.off = function (type, func, useCapture) {
-        return this.each(function(){
-           Dom.off(this,func,useCapture);
+        return this.each(function () {
+            Dom.off(this, func, useCapture);
         });
     };
-    Dom.prototype.text = function(text){
-        if(arguments.length===1){
-            return this.each(function(){
-                Dom.setText(this,text);
+    Dom.prototype.text = function (text) {
+        if (arguments.length === 1) {
+            return this.each(function () {
+                Dom.setText(this, text);
             });
-        }else{
+        } else {
             var text = [];
-            this.each(function(){
+            this.each(function () {
                 text.push(Dom.getText(this));
             });
             return text.join(",");
         }
     };
-    Dom.prototype.val = function(val){
-        if(arguments.length===1){
-            return this.each(function(){
-                Dom.setValue(this,val);
+    Dom.prototype.val = function (val) {
+        if (arguments.length === 1) {
+            return this.each(function () {
+                Dom.setValue(this, val);
             });
-        }else{
+        } else {
             var val = [];
-            this.each(function(){
+            this.each(function () {
                 val.push(Dom.getValue(this));
             });
             return val.join(",");
         }
     };
-    var attrPolyfill = {
-        text : 	{
-            get : Dom.getText,
-            set : Dom.setText
-        },
-        value : {
-            get : Dom.getValue,
-            set : Dom.setValue
-        }
-    };
-    Dom.prototype.attr = function(attr,val){
-        if(arguments.length===2){
-            return this.each(function(){
-                attrPolyfill[attr]?attrPolyfill[attr].set(this,val):Dom.setAttr(this,attr,val);
+    Dom.prototype.attr = function (attr, val) {
+        if (arguments.length === 2) {
+            return this.each(function () {
+                Dom.setAttr(this, attr, val);
             });
-        }else{
+        } else {
             var attrVal = [];
-            this.each(function(){
-                attrVal.push(attrPolyfill[attr]?attrPolyfill[attr].get(this):Dom.getAttr(this,attr));
+            this.each(function () {
+                attrVal.push(Dom.getAttr(this, attr));
             });
             return attrVal.join(",");
         }
     };
-    Dom.prototype.child = function(){
+    Dom.prototype.child = function () {
         var child = new Dom();
-        this.each(function(){
+        this.each(function () {
             child.push(Dom.getChild(this));
         });
         return child;
     };
-    Dom.prototype.parent = function(){
+    Dom.prototype.parent = function () {
         var parent = new Dom();
-        this.each(function(){
+        this.each(function () {
             parent.push(Dom.getParent(this));
         });
         return parent;
     };
-    Dom.prototype.append = function(dom){
-        return this.each(function(){
-            Dom.append(this,dom);
+    Dom.prototype.append = function (dom) {
+        return this.each(function () {
+            Dom.append(this, dom);
         });
     };
-    Dom.prototype.remove = function(){
-        return this.each(function(){
+    Dom.prototype.remove = function () {
+        return this.each(function () {
             Dom.remove(this);
         });
     };
-    Dom.prototype.replace = function(dom){
-        return this.each(function(){
-            Dom.replace(this,dom);
+    Dom.prototype.replace = function (dom) {
+        return this.each(function () {
+            Dom.replace(this, dom);
         });
     };
-    Dom.prototype.clone = function(dp){
+    Dom.prototype.clone = function (dp) {
         var clone = new Dom();
-        this.each(function(){
-            clone.push(Dom.cloneNode(this,dp));
+        this.each(function () {
+            clone.push(Dom.cloneNode(this, dp));
         });
         return clone;
     };
-    if(m.isIE8){
-        m.extend(eventHook,{
+    if (m.isIE8) {
+        m.extend(eventHook, {
             input: "propertychange"
         });
-        m.extend(Dom,{
-            on: function(dom,type,func){
-                var callback = function(o,h){
-                    return function(){
-                        h.apply(o,arguments);
+        m.extend(Dom, {
+            on: function (dom, type, func) {
+                var callback = function (o, h) {
+                    return function () {
+                        h.apply(o, arguments);
                     };
-                }(dom,func);
+                }(dom, func);
                 dom[m.hashCode(func)] = callback;
-                dom.attachEvent("on" + (eventHook[type]||type), callback);
+                dom.attachEvent("on" + (eventHook[type] || type), callback);
             },
-            off: function(dom,type,func){
+            off: function (dom, type, func) {
                 var callback = dom[m.hashCode(func)];
-                dom.detachEvent("on" + (eventHook[type]||type), callback);
+                dom.detachEvent("on" + (eventHook[type] || type), callback);
             },
-            getText: (function(extraTag){
-                extraTag.test = m.inject(extraTag).after("test",function(){
+            getText: (function (extraTag) {
+                extraTag.test = m.inject(extraTag).after("test", function () {
                     this.restore();
                 });
-                return function(dom){
-                    if(extraTag.test(dom.nodeName)){
+                return function (dom) {
+                    if (extraTag.test(dom.nodeName)) {
                         return dom.innerHTML;
-                    }else{
+                    } else {
                         var results = [];
-                        m.each(dom.childNodes,function(){
+                        m.each(dom.childNodes, function () {
                             results.push(this.nodeValue);
-                        },function(){return this.nodeType===3&&!isEmptyExp.test(this.nodeValue)});
+                        }, function () {
+                            return this.nodeType === 3 && !isEmptyExp.test(this.nodeValue)
+                        });
                         return results.join('');
                     }
                 };
@@ -225,8 +244,8 @@
         });
     }
     return m.extend({
-        dom: function (selector,context) {
-            return new Dom(selector,context);
+        dom: function (selector, context) {
+            return new Dom(selector, context);
         }
-    },Dom);
+    }, Dom);
 })(mirror);
--- mirror.template.js
+++ mirror.template.js
@@ -1,58 +1,340 @@
 /**
  * Created by shyy_work on 2016/12/16.
  */
-(function(m){
-    var d = m.using("mirror.dom"),
-        template = {};
-    function analyzeNode(node) {
-        var vd = new VDom(node);
-        m.each(d.getChild(node,true),function () {
-            vd.push(analyzeNode(this));
-        });
-        return vd;
-    }
-    function VDom(node){
-        this.node = node;
-    }
-    VDom.prototype = [];
-    VDom.prototype.dataBinding = function (data) {
-        var realNode = d.cloneNode(this.node);
-        m.each(this,function () {
-            d.append(realNode,this.dataBinding(data));
-        });
-        return realNode;
-    }
-    function Template(model,parent){
-        this.model = model;
-        this.parent = parent;
-        this.initd = false;
-    }
-    Template.prototype = [];
-    Template.prototype.init = function () {
-        var dom = d.valueOf(this.model)
-            ,container = this;
-        dom.each(function () {
-            container.push(analyzeNode(this));
-        });
-        this.initd = true;
-    };
-    Template.prototype.renderTo = function (selector, data) {
-        if(!this.initd){
-            this.init();
+(function (m) {
+        var ESCAPE = {
+                "n": "\n",
+                "f": "\f",
+                "r": "\r",
+                "t": "\t",
+                "v": "\v",
+                "'": "'",
+                '"': '"',
+                "{": "{",
+                "}": "}",
+                "[": "[",
+                "]": "]"
+            },
+            UNESCAPE = {
+                "\r": "\\r",
+                "\n": "\\n",
+                '"': "\"",
+                "'": "\'"
+            },
+            isEscapeChar = function (ch) {
+                return '\\' === ch;
+            },
+            isBraceLChar = function (ch) {
+                return '{' === ch;
+            },
+            isBraceRChar = function (ch) {
+                return '}' === ch;
+            },
+            isBracketLChar = function (ch) {
+                return '[' === ch;
+            },
+            isBracketRChar = function (ch) {
+                return ']' === ch;
+            },
+            isParenthesisLChar = function (ch) {
+                return '(' === ch;
+            },
+            isParenthesisRChar = function (ch) {
+                return ')' === ch;
+            },
+            isCommaChar = function (ch) {
+                return ',' === ch;
+            },
+            isDotChar = function (ch) {
+                return '.' === ch;
+            },
+            baseOnImm = function (queue, type) {
+                return queue[0].type === type;
+            },
+            baseOn = function (queue, type) {
+                for (var i = queue.length - 1; i >= 0; i--) {
+                    if (queue[i].type === type) return true;
+                }
+                return false;
+            },
+            parser = {
+                $root: function (tokenGroup) {
+                    var cv = [];
+                    m.each(tokenGroup, function () {
+                        if (m.isOne(this, 'String')) {
+                            cv.push('"' + this + '"');
+                        } else {
+                            cv.push('(' + parse(this) + ')(context)');
+                        }
+                    });
+                    return 'function(context){ return ' + cv.join('+') + ';}';
+                },
+                $expr: function (tokenGroup) {
+                    var cv = [];
+                    cv.push(
+                        'function(context){',
+                        'var data=context;'
+                    );
+                    m.each(tokenGroup, function () {
+                        if (m.isOne(this, 'String')) {
+                            cv.push('data=data["' + this + '"];');
+                        } else if (this.type === '$invoke') {
+                            cv.push('data=data(' + parse(this) + ');');
+                        } else {
+                            cv.push('data=data[(' + parse(this) + ')(context)];');
+                        }
+                    });
+                    cv.push(
+                        'return data;',
+                        '}'
+                    );
+                    return cv.join('');
+                },
+                $invoke: function (tokenGroup) {
+                    var cv = [];
+                    m.each(tokenGroup, function () {
+                        if (m.isOne(this, 'String')) {
+                            cv.push(this);
+                        } else {
+                            cv.push('(' + parse(this) + ')(context)');
+                        }
+                    });
+                    return cv.join(',');
+                }
+            },
+            parse = function (tokenGroup) {
+                return parser[tokenGroup.type](tokenGroup);
+            },
+            complie = function (text) {
+                var lexer = new Lexer(text);
+                var tokenGroup = lexer.lex().getTokenGroup();
+                var code = parse(tokenGroup);
+                return Function('return '+code)();
+            };
+
+        function Lexer(text) {
+            m.assertDefined(text);
+            this.text = text;
+            this.index = 0;
+            this.cache = [];
+            this.queue = [new TokenGroup('$root')];
+            this.lexed = false;
         }
-        var parent,child;
-        m.each(this,function () {
-            child = this.dataBinding(data);
-            d.dom(selector).each(function () {
-                parent = this;
-                d.append(parent,child);
+
+        Lexer.prototype.peek = function (i) {
+            i = i || 0;
+            return ((i = this.index + i) < this.text.length) ? this.text.charAt(i) : false;
+        }
+        Lexer.prototype.roll = function (i) {
+            i = i || 1;
+            return this.index = this.index + i;
+        }
+        Lexer.prototype.branch = function (type) {
+            this.trySnatch();
+            var tokenGroup = new TokenGroup(type);
+            this.queue[0].push(tokenGroup);
+            this.queue.unshift(tokenGroup);
+        }
+        Lexer.prototype.base = function (type) {
+            this.trySnatch();
+            var tokenGroup = this.queue.shift();
+            if (tokenGroup.type !== type) {
+                throw 'lexer error';
+            }
+        }
+        Lexer.prototype.baseOn = function (type, imm) {
+            return imm ? baseOnImm(this.queue, type) : baseOn(this.queue, type);
+        }
+        Lexer.prototype.clear = function () {
+            if (!this.cache.length)throw 'lexer error';
+            try {
+                return this.cache.join("");
+            } finally {
+                this.cache.length = 0;
+            }
+        }
+        Lexer.prototype.snatch = function () {
+            this.queue[0].push(this.clear());
+        }
+        Lexer.prototype.trySnatch = function () {
+            if (this.cache.length) {
+                this.snatch();
+            }
+        }
+        Lexer.prototype.lex = function () {
+            if (this.lexed) return this;
+            var ch, needEscape;
+            while ((ch = this.peek()) !== false) {
+                if (needEscape) {
+                    needEscape = false;
+                    if (ESCAPE[ch]) {
+                        this.cache.push(ESCAPE[ch]);
+                    } else {
+                        this.cache.push('\\', ch);
+                    }
+                    this.roll();
+                } else {
+                    if (isEscapeChar(ch)) {
+                        needEscape = true;
+                        this.roll();
+                    } else if (isBraceLChar(ch)) {
+                        ch = this.peek(1);
+                        if (isBraceLChar(ch)) {
+                            this.branch('$expr');
+                            this.roll(2);
+                        } else {
+                            throw 'unsupported'
+                        }
+                    } else if (isBraceRChar(ch)) {
+                        ch = this.peek(1);
+                        if (isBraceRChar(ch)) {
+                            this.base('$expr');
+                            this.roll(2);
+                        } else {
+                            throw 'unsupported'
+                        }
+                    } else {
+                        if (this.baseOn('$expr')) {
+                            if (isDotChar(ch)) {
+                                this.snatch();
+                            } else if (isBracketLChar(ch)) {
+                                this.branch('$root');
+                            } else if (isBracketRChar(ch)) {
+                                this.base('$root');
+                            } else if (isParenthesisLChar(ch)) {
+                                this.branch('$invoke')
+                            } else if (isParenthesisRChar(ch)) {
+                                this.base('$invoke');
+                            } else if (isCommaChar(ch) && this.baseOn('$invoke', true)) {
+                                this.trySnatch();
+                            } else {
+                                this.cache.push(ch);
+                            }
+                        } else {
+                            this.cache.push(UNESCAPE[ch]||ch);
+                        }
+                        this.roll();
+                    }
+                }
+            }
+            this.trySnatch();
+            this.lexed = true;
+            return this;
+        }
+        Lexer.prototype.getTokenGroup = function () {
+            if (this.queue.length !== 1 && !this.base('$root', true)) {
+                throw 'lexer error';
+            }
+            return this.queue[0];
+        }
+        function TokenGroup(type) {
+            this.type = type;
+        }
+
+        TokenGroup.prototype = [];
+
+        var d = m.using("mirror.dom"),
+            template = {},
+            attrHook = {
+                '*': ['id', 'class', 'style', 'title', 'accesskey', 'dir', 'lang', 'tabindex'],
+                'a': ['href', 'target'],
+                'img': ['src', 'width', 'height', 'alt', 'border'],
+                'table': ['width', 'border', 'cellpadding', 'cellspacing'],
+                'thead': ['align', 'char', 'charoff', 'valign'],
+                'tbody': ['align', 'char', 'charoff', 'valign'],
+                'tfoot': ['align', 'char', 'charoff', 'valign'],
+                'tr': ['align', 'char', 'charoff', 'valign'],
+                'td': ['abbr', 'align', 'axis', 'char', 'charoff', 'colspan', 'headers', 'rowspan', 'scope', 'valign'],
+                'th': ['abbr', 'align', 'axis', 'char', 'charoff', 'colspan', 'headers', 'rowspan', 'scope', 'valign'],
+                'form': ['action', 'name', 'target', 'method', 'enctype'],
+                'label': ['for'],
+                'input': ['type', 'name', 'value', 'checked', 'disabled', 'src']
+            },
+            $textNode = '#text';
+
+        function analyzeNode(node) {
+            var vd = new VDom(node);
+            if ($textNode.includes(vd.name)) {
+                vd.initd.push({
+                    name: 'text',
+                    expr: complie(d.getText(node))
+                });
+            } else {
+                m.each(attrHook['*'], function () {
+                    if (d.hasAttr(node, this)) {
+                        vd.initd.push({
+                            name: vd.name,
+                            expr: complie(d.getAttr(node, this))
+                        });
+                    }
+                });
+                if (attrHook[vd.name]) {
+                    m.each(attrHook[vd.name], function () {
+                        if (d.hasAttr(node, this)) {
+                            vd.initd.push({
+                                name: vd.name,
+                                expr: complie(d.getAttr(node, this))
+                            });
+                        }
+                    });
+                }
+                m.each(d.getChild(node, true), function () {
+                    vd.push(analyzeNode(this));
+                });
+            }
+            return vd;
+        }
+
+        function VDom(node) {
+            this.node = node;
+            this.name = node.nodeName;
+            this.initd = [];
+
+        }
+
+        VDom.prototype = [];
+        VDom.prototype.dataBinding = function (data) {
+            var realNode = d.cloneNode(this.node);
+            m.each(this.initd,function () {
+                d.setAttr(realNode,this.name,this.expr(data));
             });
-        })
-    };
-    d.dom("script[type='text/template']").each(function () {
-        var id = d.getAttr(this,"id");
-        m.assertDefined(id);
-        template[id] = new Template(m.trim(d.getText(this)));
-    });
-    return template;
-})(mirror);
+            m.each(this, function () {
+                d.append(realNode, this.dataBinding(data));
+            });
+            return realNode;
+        }
+        function Template(model, parent) {
+            this.model = model;
+            this.parent = parent;
+            this.initd = false;
+        }
+
+        Template.prototype = [];
+        Template.prototype.init = function () {
+            var dom = d.valueOf(this.model)
+                , container = this;
+            dom.each(function () {
+                container.push(analyzeNode(this));
+            });
+            this.initd = true;
+        };
+        Template.prototype.renderTo = function (selector, data) {
+            if (!this.initd) {
+                this.init();
+            }
+            var parent, child;
+            m.each(this, function () {
+                child = this.dataBinding(data);
+                d.dom(selector).each(function () {
+                    parent = this;
+                    d.append(parent, child);
+                });
+            })
+        };
+        d.dom("script[type='text/template']").each(function () {
+            var id = d.getAttr(this, "id");
+            m.assertDefined(id);
+            template[id] = new Template(m.trim(d.getText(this)));
+        });
+        return template;
+    })(mirror);
